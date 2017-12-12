857ecc1dccd43f6347468f3e29919824a4e51964
whd <547379726@qq.com>
2017-12-12 13:49:32 +0800

    新工作提交

---
+++ xm/whd_pt/src/main/java/com/pt/WhdPtApplication.java
@@ -0,0 +1,12 @@
+package com.pt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhdPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhdPtApplication.class, args);
+	}
+}
